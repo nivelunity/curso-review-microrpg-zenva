c41b5521addff1e4514b0b9219711d05cf635a44
Gaston Caminiti <gastoncaminiti@outlook.com>
2024-07-26 18:17:35 -0300

    Player Script: Move Method

--- Micro-RPG/Assets/Scripts/Player.cs
+++ Micro-RPG/Assets/Scripts/Player.cs
@@ -14,12 +14,29 @@ public class Player : MonoBehaviour
     public Sprite leftSprite;
     public Sprite rightSprite;
 
-    private Vector3 facingDirection;
+    private Vector2 facingDirection;
     private Rigidbody2D rig;
 
     private void Awake()
     {
         rig = GetComponent<Rigidbody2D>();
     }
-    
+
+    private void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector2 vel = new Vector2(x, y);
+
+        if (vel.magnitude != 0)
+            facingDirection = vel;
+
+        rig.velocity = vel * moveSpeed;
+    }
 }
