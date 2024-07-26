d0efcf0466bcbfd6f21e0e362723ca0248bc9253
Gaston Caminiti <gastoncaminiti@outlook.com>
2024-07-26 17:59:38 -0300

    Player GO y Player Script

---
+++ Micro-RPG/Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Stats")]
+    public float moveSpeed;
+
+    [Header("Sprites")] 
+    public Sprite downSprite;
+    public Sprite upSprite;
+    public Sprite leftSprite;
+    public Sprite rightSprite;
+
+}
