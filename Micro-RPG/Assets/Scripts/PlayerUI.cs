917cc73d46590ccb9e12826ce4a47252d5e18088
Gaston Caminiti <gastoncaminiti@outlook.com>
2024-07-31 10:45:56 -0300

    Interactable Script: SetInteractText and DisableInteractText Method

--- Micro-RPG/Assets/Scripts/PlayerUI.cs
+++ Micro-RPG/Assets/Scripts/PlayerUI.cs
@@ -9,6 +9,7 @@ public class PlayerUI : MonoBehaviour
 {
    public TextMeshProUGUI levelText;
    public TextMeshProUGUI inventoryText;
+   public TextMeshProUGUI interactText;
    public Image healthBarFill;
    public Image xpBarFill;
 
@@ -31,5 +32,18 @@ public class PlayerUI : MonoBehaviour
    public void UpdateXpBar()
    {
       xpBarFill.fillAmount =  (float)player.curXp / (float)player.xpToNextLevel;
+   }
+
+   public void SetInteractText(Vector3 pos, string text)
+   {
+      interactText.gameObject.SetActive(true);
+      interactText.text = text;
+      interactText.transform.position = Camera.main.WorldToScreenPoint(pos + Vector3.up);
+   }
+
+   public void DisableInteractText()
+   {
+      if(interactText.gameObject.activeInHierarchy)
+         interactText.gameObject.SetActive(false);
    }
 }
