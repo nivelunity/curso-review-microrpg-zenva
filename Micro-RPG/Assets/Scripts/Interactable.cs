284111a477c40d170eab6fe7f99066eea2dd0d85
Gaston Caminiti <gastoncaminiti@outlook.com>
2024-07-31 10:32:21 -0300

    Interactable Script: UnityEvent onInteract

--- Micro-RPG/Assets/Scripts/Interactable.cs
+++ Micro-RPG/Assets/Scripts/Interactable.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Interactable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   public string interactDescription;
+   public UnityEvent onInteract;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public void Inteact()
+   {
+      if(onInteract != null)
+         onInteract.Invoke();
+   }
 }
